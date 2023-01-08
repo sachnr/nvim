b84da739efd597859a805f77347b5325f21b070b
sachnr <sachinrawat544@gmail.com>
2023-01-08 18:43:56 +0530

    yo2

--- lua/config/null-ls.lua
+++ lua/config/null-ls.lua
@@ -6,9 +6,11 @@ end
 
 local handlers = require("config.lsp.handlers")
 local on_attach = handlers.on_attach
+local capablities = handlers.capablities()
 
 null_ls.setup({
 	on_attach = on_attach,
+	capablities = capablities,
 	sources = {
 		null_ls.builtins.diagnostics.eslint_d,
 		null_ls.builtins.code_actions.eslint_d,
